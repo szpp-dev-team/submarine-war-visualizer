c30f0650546b38a8c549f722d2c797212c3b316e
arumakan1727 <aluminumCan7271@gmail.com>
2021-01-04 16:42:04 +0900

    add: 基盤

---
+++ js/main.ts
@@ -0,0 +1,148 @@
+const CANVAS_WIDTH = 860;
+const CANVAS_HEIGHT = 640;
+const N = 5;
+
+
+abstract class Geometry {
+    static centerPos(elementLength: number, containerLength: number): number {
+        return (containerLength - elementLength) / 2;
+    }
+}
+
+
+abstract class MyColor {
+    static readonly lightGray = '#eee';
+    static readonly darkGray = '#333';
+    static readonly submarine_red = '#CB2400';
+    static readonly submarine_blue = '#236CCA'
+}
+
+interface Scene {
+    sceneChanger: SceneChanger;
+    setup: (sceneChanger_: SceneChanger) => void;
+    tearDown: () => void;
+    update: (timestamp: number) => void;
+    draw: (ctx: CanvasRenderingContext2D) => void;
+}
+
+
+interface SceneChanger {
+    canvas: HTMLCanvasElement;
+    changeScene: (nextScene: Scene) => void;
+}
+
+
+class Visualizer implements SceneChanger {
+    readonly canvas: HTMLCanvasElement;
+    readonly ctx: CanvasRenderingContext2D;
+    curScene: Scene;
+
+    constructor(canvas: HTMLCanvasElement, scene: Scene) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.curScene = scene;
+
+        this.canvas.width = CANVAS_WIDTH;
+        this.canvas.height = CANVAS_HEIGHT;
+        scene.setup(this);
+
+        this.ctx.textAlign = 'center';
+    }
+
+    run(): void {
+        const animationLoop = (timestamp: number): void => {
+            this.curScene.update(timestamp);
+            this.ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+            this.curScene.draw(this.ctx);
+            requestAnimationFrame(animationLoop);
+        };
+        requestAnimationFrame(animationLoop);
+    }
+
+    changeScene(nextScene: Scene): void {
+        this.curScene.tearDown();
+        this.curScene = nextScene;
+        this.curScene.setup(this);
+    }
+}
+
+
+class TitleScene implements Scene {
+    sceneChanger: SceneChanger;
+    private readonly clickHandler: EventListener;
+
+    constructor() {
+        this.clickHandler = () => {
+            console.log("[TitleScene#clickHandler] Clicked!");
+            let battleScene = new BattleScene();
+            this.sceneChanger.changeScene(battleScene);
+        };
+    }
+
+    setup(sceneChanger: SceneChanger): void {
+        this.sceneChanger = sceneChanger;
+        this.sceneChanger.canvas.addEventListener('click', this.clickHandler, false);
+    }
+
+    tearDown(): void {
+        this.sceneChanger.canvas.removeEventListener('click', this.clickHandler, false);
+    }
+
+    update(timestamp: number): void {
+    }
+
+    draw(ctx: CanvasRenderingContext2D): void {
+        this._draw_back(ctx);
+        this._draw_message(ctx);
+    }
+
+    private _draw_message(ctx: CanvasRenderingContext2D): void {
+        ctx.fillStyle = MyColor.darkGray;
+        ctx.font = '32px sans-serif'
+        const msg = "潜水艦の初期配置を入力してOKボタンを押してください";
+        ctx.fillText(msg, CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    }
+
+    private _draw_back(ctx: CanvasRenderingContext2D): void {
+        ctx.fillStyle = MyColor.lightGray;
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+
+
+class BattleScene implements Scene {
+    sceneChanger: SceneChanger;
+
+    setup(sceneChanger: SceneChanger): void {
+    }
+
+    tearDown(): void {
+    }
+
+    update(timestamp: number): void {
+    }
+
+    draw(ctx: CanvasRenderingContext2D): void {
+        this._draw_back(ctx);
+        this._draw_message(ctx);
+    }
+
+    private _draw_message(ctx: CanvasRenderingContext2D): void {
+        ctx.fillStyle = MyColor.darkGray;
+        ctx.font = '32px sans-serif'
+        const msg = "Battle Scene";
+        ctx.fillText(msg, CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    }
+
+    private _draw_back(ctx: CanvasRenderingContext2D): void {
+        ctx.fillStyle = MyColor.lightGray;
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
+    const titleScene = new TitleScene();
+    const visualizer = new Visualizer(canvas, titleScene);
+    visualizer.run();
+});
